0b4a17301e41f346800ee62092f566dfad699386
MrAdamTaylor <126983829+MrAdamTaylor@users.noreply.github.com>
2024-03-28 23:48:07 +0300

    -Aded GroupMoving
    -Aded Group Moving
    -Aded Shark

--- Assets/Scripts/FlockSimulation/Flock.cs
+++ Assets/Scripts/FlockSimulation/Flock.cs
@@ -26,6 +26,44 @@ namespace FlockSimulation
 
             Vector3 vcentre = Vector3.zero;
             Vector3 vavoid = Vector3.zero;
+
+            float gSpeed = 0.01f;
+            float nDistance;
+            int groupSize = 0;
+
+            foreach (GameObject go in gos)
+            {
+                if (go != this.gameObject)
+                {
+                    nDistance = Vector3.Distance(go.transform.position, this.transform.position);
+                    if (nDistance <= FlockManager.FM.NeighbourDistance)
+                    {
+                        vcentre += go.transform.position;
+                        groupSize++;
+
+                        if (nDistance < 1.0f)
+                        {
+                            vavoid = vavoid + (this.transform.position - go.transform.position);
+                        }
+
+                        Flock anotherFlock = go.GetComponent<Flock>();
+                        gSpeed = gSpeed + anotherFlock._speed;
+                    }
+                }
+            }
+
+            if (groupSize > 0)
+            {
+                vcentre = vcentre / groupSize;
+                _speed = gSpeed / groupSize;
+
+                Vector3 direction = (vcentre + vavoid) - transform.position;
+                if (direction != Vector3.zero)
+                {
+                    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction),
+                        FlockManager.FM.RotationSpeed * Time.deltaTime);
+                }
+            }
         }
     }
 }
